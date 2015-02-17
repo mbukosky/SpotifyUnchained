2e8d408991f72d06bb5cb6e1a253c1214c38b978
Michael Bukosky <mbukosky@gmail.com>
2015-02-17 15:59:04 -0500

    Wired up the sync and save
    - Added a TODO for the schedule job

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -1,7 +1,20 @@
 'use strict';
 
 module.exports = function(app) {
-	// Root routing
-	var core = require('../../app/controllers/core.server.controller');
-	app.route('/').get(core.index);
+  // Root routing
+  var core = require('../../app/controllers/core.server.controller');
+  app.route('/').get(core.index);
+
+  //TODO: install node-schedule
+  //var schedule = require('node-schedule');
+
+  //var rule = new schedule.RecurrenceRule();
+  //rule.second = [0, 20, 40];
+
+  // var j = schedule.scheduleJob(rule, function(){
+  //     spotify.list();
+  // });
+
+  var spotify = require('../../app/controllers/spotify.server.controller');
+  spotify.sync();
 };
