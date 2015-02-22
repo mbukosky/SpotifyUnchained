c17747ea1dc4c0e4226ad83ed983462381bf16b1
Michael Bukosky <mbukosky@gmail.com>
2015-02-22 08:34:00 -0500

    Added accordion dropdown for playlists

--- public/modules/core/controllers/new-tuesday-playlist.client.controller.js
+++ public/modules/core/controllers/new-tuesday-playlist.client.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('core').controller('NewTuesdayPlaylistController', ['$scope', 'Spotify',
   function($scope, Spotify) {
-
+    $scope.oneAtATime = true;
     $scope.data = Spotify.query();
 
   }
