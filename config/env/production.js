6a46b9368860c3109b7937c4c70f958711e4f49d
Michael Bukosky <mbukosky@gmail.com>
2015-02-28 16:57:41 -0500

    HOTFIX: Adding new angular-spotify to production config

--- config/env/production.js
+++ config/env/production.js
@@ -17,7 +17,8 @@ module.exports = {
         'public/lib/angular-sanitize/angular-sanitize.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
         'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+        'public/lib/angular-spotify/dist/angular-spotify.min.js'
       ]
     },
     css: 'public/dist/application.min.css',
