fda31f0771b0ab463ed1167e6bae2bf4e0784c28
Michael Bukosky <mbukosky@gmail.com>
2015-02-28 16:12:57 -0500

    Trying out different ideas
    - WIP

--- public/modules/core/controllers/new-tuesday-playlist.client.controller.js
+++ public/modules/core/controllers/new-tuesday-playlist.client.controller.js
@@ -1,14 +1,53 @@
 'use strict';
 
-angular.module('core').controller('NewTuesdayPlaylistController', ['$scope', 'SpotifyPlaylist',
-  function($scope, SpotifyPlaylist) {
+angular.module('core').controller('NewTuesdayPlaylistController', ['$scope', 'SpotifyPlaylist', 'Spotify', 'localStorageService',
+  function($scope, SpotifyPlaylist, Spotify, localStorageService) {
     $scope.oneAtATime = true;
     $scope.data = SpotifyPlaylist.query();
-    $scope.saved = null;
+    $scope.user = null;
 
-    $scope.onSavePlaylist = function(title) {
-      $scope.saved = title;
+    //TODO Need to figure out how to expire the token from the cache
+    $scope.token = localStorageService.get('spotify-token');
+
+    $scope.onSavePlaylist = function(title, tracks) {
+      var uris = [];
+      angular.forEach(tracks, function(value) {
+        this.push(value.uri);
+      }, uris);
+
+      Spotify.createPlaylist($scope.user, {
+        name: title,
+        public: false
+      }).then(function(data) {
+        var playlist = data.id;
+
+        Spotify.addPlaylistTracks($scope.user, playlist, uris).then(function(data) {
+          console.log('tracks added to playlist - ' + data);
+        });
+      });
     };
+
+    // $scope.token = null;
+    $scope.login = function() {
+      Spotify.login().then(function(data) {
+        $scope.token = data;
+        localStorageService.set('spotify-token', data);
+
+        Spotify.getCurrentUser().then(function(data) {
+          $scope.user = data.id;
+        });
+      });
+    };
+
+    if (!$scope.token) {
+      $scope.login();
+    } else {
+      Spotify.setAuthToken($scope.token);
+
+      Spotify.getCurrentUser().then(function(data) {
+        $scope.user = data.id;
+      });
+    }
 
   }
 ]);
