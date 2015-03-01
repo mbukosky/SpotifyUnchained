045c2efe05dd45eac8e532459f9462e503e7b493
Michael Bukosky <mbukosky@gmail.com>
2015-02-28 22:08:52 -0500

    Cleaning up all app/article

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -14,46 +14,11 @@ module.exports = function(app) {
   app.route('/users').put(users.update);
   app.route('/users/accounts').delete(users.removeOAuthProvider);
 
-  // Setting up the users password api
-  app.route('/users/password').post(users.changePassword);
-  app.route('/auth/forgot').post(users.forgot);
-  app.route('/auth/reset/:token').get(users.validateResetToken);
-  app.route('/auth/reset/:token').post(users.reset);
-
   // Setting up the users authentication api
-  app.route('/auth/signup').post(users.signup);
   app.route('/auth/signin').post(users.signin);
   app.route('/auth/signout').get(users.signout);
 
-  // Setting the facebook oauth routes
-  app.route('/auth/facebook').get(passport.authenticate('facebook', {
-    scope: ['email']
-  }));
-  app.route('/auth/facebook/callback').get(users.oauthCallback('facebook'));
-
-  // Setting the twitter oauth routes
-  app.route('/auth/twitter').get(passport.authenticate('twitter'));
-  app.route('/auth/twitter/callback').get(users.oauthCallback('twitter'));
-
-  // Setting the google oauth routes
-  app.route('/auth/google').get(passport.authenticate('google', {
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email'
-    ]
-  }));
-  app.route('/auth/google/callback').get(users.oauthCallback('google'));
-
-  // Setting the linkedin oauth routes
-  app.route('/auth/linkedin').get(passport.authenticate('linkedin'));
-  app.route('/auth/linkedin/callback').get(users.oauthCallback('linkedin'));
-
-  // Setting the github oauth routes
-  app.route('/auth/github').get(passport.authenticate('github'));
-  app.route('/auth/github/callback').get(users.oauthCallback('github'));
-
   // Setting the spotify oauth routes
-  //playlist-read-private playlist-modify-private
   app.route('/auth/spotify').get(passport.authenticate('spotify', {
     scope: ['playlist-read-private', 'playlist-modify-private']
   }));
