dd0394b3a687ec7e40ecea0230f85d7299e05beb
Michael Bukosky <mbukosky@gmail.com>
2015-03-08 11:19:37 -0400

    Setup cron to sync playlists
    - Runs every 2 hours

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -5,16 +5,18 @@ module.exports = function(app) {
   var core = require('../../app/controllers/core.server.controller');
   app.route('/').get(core.index);
 
-  //TODO: install node-schedule
-  //var schedule = require('node-schedule');
-
-  //var rule = new schedule.RecurrenceRule();
-  //rule.second = [0, 20, 40];
-
-  // var j = schedule.scheduleJob(rule, function(){
-  //     spotify.list();
-  // });
-
+  // Kick off the first sync when the server starts up
   var spotify = require('../../app/controllers/spotify.server.controller');
   spotify.sync();
+
+  // Schedule cron to sync every 2 hours
+  var schedule = require('node-schedule');
+
+  var rule = new schedule.RecurrenceRule();
+  rule.hour = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22];
+  rule.minute = 0;
+
+  var j = schedule.scheduleJob(rule, function() {
+    spotify.sync();
+  });
 };
