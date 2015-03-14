a10c8a21b8dae5e47ee818babb17693dab1b8fa1
Michael Bukosky <mbukosky@gmail.com>
2015-03-14 08:49:34 -0400

    HOTFIX: disable exception and xhr logging

--- public/modules/core/config/logger.client.config.js
+++ public/modules/core/config/logger.client.config.js
@@ -7,9 +7,17 @@ angular.module('core').config(
     LOG_LOGGER_CONFIG.enabled = {
       global: true, //global flag to enable remote logging for all log operations
       warn: true,
-      // error: true,
+      error: true,
       info: true,
       log: true
     };
+  }
+).config(
+  function(EXCEPTION_LOGGER_CONFIG) {
+    EXCEPTION_LOGGER_CONFIG.enabled = false; //disables the exception logger
+  }
+).config(
+  function(XHR_LOGGER_CONFIG) {
+    XHR_LOGGER_CONFIG.enabled = false; //disables the xhr-logger
   }
 );
