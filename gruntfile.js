5fbc52a93a3492d90ef4e1cf5e98b5e54c6dd5ec
Unknown <lmeley@gmail.com>
2019-07-31 09:52:30 -0400

    Node 10 upgrade

--- gruntfile.js
+++ gruntfile.js
@@ -92,19 +92,6 @@ module.exports = function(grunt) {
         }
       }
     },
-    'node-inspector': {
-      custom: {
-        options: {
-          'web-port': 1337,
-          'web-host': 'localhost',
-          'debug-port': 5858,
-          'save-live-edit': true,
-          'no-preload': true,
-          'stack-trace-limit': 50,
-          'hidden': []
-        }
-      }
-    },
     ngAnnotate: {
       production: {
         files: {
@@ -114,7 +101,7 @@ module.exports = function(grunt) {
     },
     concurrent: {
       default: ['nodemon', 'watch'],
-      debug: ['nodemon', 'watch', 'node-inspector'],
+      debug: ['nodemon', 'watch'],
       options: {
         logConcurrentOutput: true,
         limit: 10
