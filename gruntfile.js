91127261bb7dfa632e508182e8125ef57910e470
Joshua Goldberg <joshg4096@gmail.com>
2020-08-20 09:10:37 -0700

    Change --debug to --inspect

--- gruntfile.js
+++ gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
       dev: {
         script: 'server.js',
         options: {
-          nodeArgs: ['--debug'],
+          nodeArgs: ['--inspect'],
           ext: 'js,html',
           watch: watchFiles.serverViews.concat(watchFiles.serverJS)
         }
