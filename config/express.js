cd3236f972384a219fbe697fd22ecd18859c08e6
Joshua Goldberg <joshg4096@gmail.com>
2020-08-23 09:34:56 -0400

    Generate new angular project

--- config/express.js
+++ config/express.js
@@ -110,7 +110,7 @@ module.exports = function(mongoose) {
   app.disable('x-powered-by');
 
   // Setting the app router and static folder
-  app.use(express.static(path.resolve('./public')));
+  app.use(express.static(path.resolve('./dist/SpotifyUnchained')));
 
   // Globbing routing files
   config.getGlobbedFiles('./app/routes/**/*.js').forEach(function(routePath) {
--- karma.conf.js
+++ karma.conf.js
@@ -1,52 +1,32 @@
-'use strict';
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
-/**
- * Module dependencies.
- */
-var applicationConfiguration = require('./config/config');
-
-// Karma configuration
-module.exports = function(config) {
-	config.set({
-		// Frameworks to use
-		frameworks: ['jasmine'],
-
-		// List of files / patterns to load in the browser
-		files: applicationConfiguration.assets.lib.js.concat(applicationConfiguration.assets.js, applicationConfiguration.assets.tests),
-
-		// Test results reporter to use
-		// Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-		//reporters: ['progress'],
-		reporters: ['progress'],
-
-		// Web server port
-		port: 9876,
-
-		// Enable / disable colors in the output (reporters and logs)
-		colors: true,
-
-		// Level of logging
-		// Possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
-
-		// Enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
-
-		// Start these browsers, currently available:
-		// - Chrome
-		// - ChromeCanary
-		// - Firefox
-		// - Opera
-		// - Safari (only Mac)
-		// - PhantomJS
-		// - IE (only Windows)
-		browsers: ['PhantomJS'],
-
-		// If browser does not capture in given timeout [ms], kill it
-		captureTimeout: 60000,
-
-		// Continuous Integration mode
-		// If true, it capture browsers, run tests and exit
-		singleRun: true
-	});
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/SpotifyUnchained'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
 };
