bcd7ca4d139c2dcf4521f9a8ae0da0881b573561
Joshua Goldberg <joshg4096@gmail.com>
2021-01-12 09:04:38 -0500

    Upgrade Angular version

--- src/app/playlist-table/playlist-table.component.spec.ts
+++ src/app/playlist-table/playlist-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PlaylistTableComponent } from './playlist-table.component';
 import { PlaylistService } from '../playlist.service';
@@ -12,7 +12,7 @@ xdescribe('PlaylistTableComponent', () => {
   const mockPlaylistResponse = {};
   playlistService.getPlaylists.and.returnValue(of(mockPlaylistResponse));
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [PlaylistTableComponent],
       providers: [{ provide: PlaylistService, useValue: playlistService }]
